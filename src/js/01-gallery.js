609667ef1e9d7e4b3fd382f8d9d10c7ca02fe4ca
andrzej-falkowski <andrzejfalkowski88@gmail.com>
2023-11-18 20:32:56 +0100

    Added Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(element) {
+  return element
+    .map(
+      ({ preview, original, description }) =>
+        `<li>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+    )
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox('ul.gallery a', {
+captionPosition:'bottom',
+captionDelay: 250,
+captionsData: 'alt'});
