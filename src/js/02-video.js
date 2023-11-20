ae237893338c027dc7951c0a7a59cda544da68f6
andrzej-falkowski <andrzejfalkowski88@gmail.com>
2023-11-20 20:26:55 +0100

    Added task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
-import Vimeo from "@vimeo/player";
+import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
 
 const iframe = document.querySelector("#vimeo-player");
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on("timeupdate", throttle((event) => {
     localStorage.setItem("videoplayer-current-time", event.seconds)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector(".feedback-form input");
+const messageInput = document.querySelector(".feedback-form textarea");
+
+const localStorageKey = "feedback-form-state";
+
+const throttled = throttle(function (email, message) {
+    localStorage.setItem(
+      localStorageKey,
+      JSON.stringify({ email, message })
+    );
+  }, 500);
+
+form.addEventListener("input", (event) => {
+  throttled(emailInput.value, messageInput.value);
+});
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log({ email: emailInput.value, message: messageInput.value });
+  localStorage.removeItem(localStorageKey);
+  event.currentTarget.reset();
+});
+
+const saved = localStorage.getItem(localStorageKey);
+
+if (saved) {
+  const { email, message } = JSON.parse(saved);
+  emailInput.value = email;
+  messageInput.value = message;
+}
